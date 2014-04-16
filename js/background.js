adf6523c981ad8770056e974071c8160cc97ee85
Alex Young <alex@helicoid.net>
2014-04-16 14:24:41 +0100

    Update background.js

--- js/background.js
+++ js/background.js
@@ -14,17 +14,6 @@ function onClickHandler(info) {
   details.pageUrl = info.pageUrl;
   details.text = info.selectionText;
 
-
- // var information;
- //  for (var key in info) {
- //    if (info.hasOwnProperty(key)) {
- //      information += ' ' + key
- //    };
- //  };
-
- //  console.log(information); //undefined editable linkUrl menuItemId pageUrl selectionText 
- //  console.log(details);
-
   chrome.windows.create({ url: 'window.html', type: 'popup', width: 300, height: 200 }, function() {
     chrome.runtime.sendMessage({ details: details }, function(response) {});
   });
